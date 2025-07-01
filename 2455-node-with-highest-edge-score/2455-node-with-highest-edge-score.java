098aecf6cced1cbc0cd974c2120c26f8166de91a
Aayush Pandey <aayush.pandey7489@gmail.com>
2025-07-01 13:02:41 +0530

    Time: 3 ms (100%), Space: 58.2 MB (28.9%) - LeetHub

--- 2455-node-with-highest-edge-score/2455-node-with-highest-edge-score.java
+++ 2455-node-with-highest-edge-score/2455-node-with-highest-edge-score.java
@@ -1,39 +1,24 @@
 class Solution {
     public int edgeScore(int[] edges) {
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        int n = edges.length;
-
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(int i=0;i<n;i++){
-            adj.get(edges[i]).add(i);
-        }
         
+        int n = edges.length;
+        long[] arr = new long[n];
 
-        List<Long> ans = new ArrayList<>();
         for(int i=0;i<n;i++){
-            long sum = 0;
-            for(int j=0;j<adj.get(i).size();j++){
-                sum += adj.get(i).get(j);
-            }
-
-            ans.add(sum);
+            arr[edges[i]] += i;
         }
 
-        long max = Collections.max(ans);
 
-
-        int m = Integer.MIN_VALUE;
-        for(int i=n-1;i>=0;i--){
-            if(ans.get(i) == max){
-                m = i;
+        long res = -1;
+        int ans = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i] > res){
+                res = arr[i];
+                ans = i;
             }
         }
 
-        return m;
+        return ans;
     }
 }
