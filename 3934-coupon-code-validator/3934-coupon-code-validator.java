945449e3411a9ff12762ef5a225ec3c0a685ccbd
Aayush Pandey <aayush.pandey7489@gmail.com>
2025-07-07 00:04:34 +0530

    Time: 5 ms (98.85%), Space: 45.6 MB (86.71%) - LeetHub

---
+++ 3934-coupon-code-validator/3934-coupon-code-validator.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        
+        List<String> ans = new ArrayList<>();
+
+        int n  = code.length;
+
+        PriorityQueue<String[]> pq = new PriorityQueue<>((a,b) -> {
+            int cmp = a[0].compareTo(b[0]);
+            if (cmp == 0) {
+                return a[1].compareTo(b[1]);
+            }
+            return cmp;
+        });
+
+        for(int i = 0; i<n;i++){
+            if(isActive[i]){
+                String c = code[i];
+
+
+                if(businessLine[i].equals("electronics") || businessLine[i].equals("grocery") || 
+                businessLine[i].equals("pharmacy") || businessLine[i].equals("restaurant")){
+
+                    boolean is = false;
+                    for(char ch : c.toCharArray()){
+                        if(ch >= '0' && ch <= '9' || ch >= 'a' && ch <= 'z' 
+                        || ch >= 'A' && ch <= 'Z' || ch == '_'){
+                            is = true;
+                        }else{
+                            is = false;
+                            break;
+                        }
+                    }
+                    
+                    if(is){
+                        pq.offer(new String[]{businessLine[i] , c});
+                    }
+
+                }else{
+                    continue;
+                }
+            }else{
+                continue;
+            }
+        }
+
+        while(!pq.isEmpty()){
+            String[] s = pq.poll();
+            ans.add(s[1]);
+        }
+
+        return ans;
+    }
+}
