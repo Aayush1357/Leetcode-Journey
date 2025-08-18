fd6e953fd33a9aa658ad736b4dc4c56cddb201c7
Aayush Pandey <aayush.pandey7489@gmail.com>
2025-08-19 02:38:01 +0530

    Time: 24 ms (93.36%), Space: 55.1 MB (80.04%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -5,21 +5,23 @@ class Solution {
         int[][] dp = new int[n][n];
 
         for(int[] r : dp){
-            Arrays.fill(r, 0);
+            Arrays.fill(r, -1);
         }
 
+        return f(0 , n-1 , s , dp);
 
-        for(int i=n-1;i>=0;i--){
-            dp[i][i] = 1;
-            for(int j=i+1;j<n;j++){
-                if(s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = 2 + dp[i+1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i][j-1] , dp[i+1][j]);
-                }
-            }
-        }
-        return dp[0][n-1];
+        // for(int i=n-1;i>=0;i--){
+        //     dp[i][i] = 1;
+        //     for(int j=i+1;j<n;j++){
+        //         if(s.charAt(i) == s.charAt(j)){
+        //             dp[i][j] = 2 + dp[i+1][j-1];
+        //         }else{
+        //             dp[i][j] = Math.max(dp[i][j-1] , dp[i+1][j]);
+        //         }
+        //     }
+        // }
+        // return dp[0][n-1];
+        
     }
 
     public static int f(int i , int j , String s , int[][] dp){
