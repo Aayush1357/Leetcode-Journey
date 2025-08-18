3abd6ffcf0ac016e314a7c1c69b9c8d07dea7df8
Aayush Pandey <aayush.pandey7489@gmail.com>
2025-08-19 02:15:53 +0530

    Time: 20 ms (72.74%), Space: 50.8 MB (77.14%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -9,7 +9,28 @@ class Solution {
             Arrays.fill(r , -1);
         }
 
-        return f(n , m, text1 , text2 , dp);
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0; 
+        }
+
+        for(int i=0;i<=m;i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1] , dp[i-1][j]);
+                }
+
+            }
+        }
+
+
+        return dp[n][m];
+        // return f(n , m, text1 , text2 , dp);
 
     }
 
